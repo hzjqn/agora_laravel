ba1a25c1dd53ea6ea6536a8c9fa716f18d67a7a2
hzjqn <hzdes.95@gmail.com>
2018-11-09 00:45:41 -0300

    se añadio soporte para lenguajes (es-AR)

---
+++ resources/lang/es-AR/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'La contraseña debe contener al menos 6 caracteres y corresponder con la confirmación de la misma.',
+    'reset' => 'Tu contraseña ha sido renovada.',
+    'sent' => 'Hemos enviado un email con el link para que renueves tu contraseña!',
+    'token' => 'Este token de renovacion de contraseña es invalido.',
+    'user' => "No pudimos encontrar un usuario con esa direccion de correo electronico ni nombre de usuario.",
+
+];
