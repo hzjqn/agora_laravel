c89b0f859c41de221847cb8a3a67d080f9f823eb
hzjqn <hzdes.95@gmail.com>
2018-12-04 08:45:10 -0300

    a

--- public/js/app.js
+++ public/js/app.js
@@ -47399,7 +47399,7 @@ if (false) {
 /* 43 */
 /***/ (function(module, exports) {
 
-throw new Error("Module build failed: ModuleBuildError: Module build failed: Error: ENOENT: no such file or directory, open 'C:\\Users\\hz\\dev\\agora\\resources\\sass\\app.sass'\n    at runLoaders (C:\\Users\\hz\\dev\\agora\\node_modules\\webpack\\lib\\NormalModule.js:195:19)\n    at C:\\Users\\hz\\dev\\agora\\node_modules\\loader-runner\\lib\\LoaderRunner.js:364:11\n    at C:\\Users\\hz\\dev\\agora\\node_modules\\loader-runner\\lib\\LoaderRunner.js:200:19\n    at C:\\Users\\hz\\dev\\agora\\node_modules\\enhanced-resolve\\lib\\CachedInputFileSystem.js:70:14\n    at process._tickCallback (internal/process/next_tick.js:61:11)");
+// removed by extract-text-webpack-plugin
 
 /***/ })
 /******/ ]);
---
+++ resources/views/editor.blade.php
@@ -0,0 +1,46 @@
+@extends('layouts.main')
+
+@section('navbar', '')
+@section('botbar', '')
+
+@section('head-js')
+@endsection
+
+@section('content')
+<div class="container">
+    <form class="editor solid-form" id="editorMain" autocomplete="off">
+        <div class="input-case title">
+            <input type="text" autocomplete="false" name="user_id"  value="{{ Auth::user()->id }}" hidden>
+            <input data-max-chars="190" type="text" autocomplete="false" name="title" class="editor title-input solid-title-input" value="{{ old('title') ?? __('') }}">
+            <label for="title">{{ __('Title') }}</label>
+        <span class="length-indicator"><i class="counter">0/190</i>&nbsp;— {{ __('Title character limit') }}</span>
+        </div>
+        <div id="editorToolbar" class="editor toolbar">
+
+        </div>
+        <div id="article" autofocus="true" class="editor-canvas editable" placeholder="{{ __("Write here.") }}">
+        </div>
+        <label for="cover">Subir una foto de portada</label><br>
+        <input type="file" name="cover" id="cover">
+        <div class="editor-form">
+            <button id="publishBtn" class="btn objective">
+                {{ __('Publish') }}
+            </button>
+            <button id="saveBtn" class="btn sidequest">
+                {{ __('Save as draft') }}
+            </button>
+        </div>
+    </form>
+</div>
+@endsection
+
+@section('js')
+    <script src=" {{ asset('js/solid.js') }} "></script>
+    <script src="//cdn.jsdelivr.net/npm/medium-editor@latest/dist/js/medium-editor.min.js"></script>
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/medium-editor@latest/dist/css/medium-editor.min.css" type="text/css" media="screen" charset="utf-8">
+    <script src=" {{ asset('js/editor.js') }} "></script>
+@endsection
+
+@section('head-css')
+    <link rel="stylesheet" href=" {{ asset('css/beagle.min.css') }}">
+@endsection
